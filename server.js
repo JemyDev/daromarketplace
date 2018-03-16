e5d94f490df28bc255535f9b4bcf314da39bb4ff
Jérémy Delhaie <Elise@macbook-pro-de-jeremy.home>
2018-03-16 17:13:56 +0100

    Deploy

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 var express = require('express');
 var path = require('path');
+var history = require('connect-history-api-fallback');
 var serveStatic = require('serve-static');
-
 app = express();
+
 app.use(serveStatic(__dirname + "/dist"));
-app.use((req, res) => {
-    res.status(404).send('/404');
-});
+app.use(history({
+    disableDotRule: true,
+    verbose: true
+}));
+app.use(serveStatic(__dirname + "/dist"));
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
