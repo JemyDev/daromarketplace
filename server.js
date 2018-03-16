69a3fc1ffcba69747a6a6db35bea244c4432e23d
Jérémy Delhaie <Elise@macbook-pro-de-jeremy.home>
2018-03-16 16:56:03 +0100

    Deploy

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ var serveStatic = require('serve-static');
 
 app = express();
 app.use(serveStatic(__dirname + "/dist"));
+app.use((req, res) => {
+    res.status(404).send('/404');
+});
 var port = process.env.PORT || 5000;
 app.listen(port);
 console.log('server started '+ port);
